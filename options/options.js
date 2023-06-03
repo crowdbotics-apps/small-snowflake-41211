dacdcd4c11dda4ef889449bd24deb336309ce7d1
Crowdbotics <team@crowdbotics.com>
2023-06-03 04:06:27 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "small-snowflake-41211",
+  url: "https://small-snowflake-41211.botics.co",
+  api: "https://small-snowflake-41211.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
